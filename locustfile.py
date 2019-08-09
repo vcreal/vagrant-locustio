53227170be431f6cb1f3fbd55ab4ddf1663e18d9
Cassey Real <creal@zetaglobal.com>
2019-08-09 19:07:16 +0800

    Refactored users and json_body

--- locustfile.py
+++ locustfile.py
@@ -6,9 +6,24 @@ class MyTaskSet(TaskSet):
     username = "api"
     password = "5b8ee56cf2d2635644a19ff1ba01d6f1"
 
-    users_list = ["user3@test.com", "user1@test.com", "user2@test.com"]
-    users_count = users_list.__len__()
-    order_list = [[], [], []]
+    users_count = 1000
+    order_list = []
+    for i in range(1, users_count):
+        order_list.append([])
+
+    headers = {"accept-encoding": "gzip, deflate", "Accept": "application/json",
+                    "Content-Type": "application/json"}
+    json_body = {
+        "activity": {
+            "subscriber": {
+                "uid": ""
+            },
+            "event": "",
+            "properties": {
+                "order_id": 0
+            }
+        }
+    }
 
     @task(3)
     def add_to_cart(self):
@@ -16,13 +31,11 @@ class MyTaskSet(TaskSet):
         user_index = randint(1, self.users_count) - 1
         print("user_add_to_cart " + self.users_list[user_index] + " order#" + order)
         self.order_list[user_index].append(order)
-
-        self.headers = {"accept-encoding": "gzip, deflate", "Accept": "application/json",
-                        "Content-Type": "application/json"}
-        json_body = {"activity": {"subscriber": {"uid": self.users_list[user_index]}, "event": "add_to_cart",
-                                  "properties": {"order_id": order}}}
+        self.json_body['uid'] = "user%i@test.com" % user_index
+        self.json_body['event'] = "add_to_cart2"
+        self.json_body['order_id'] = order
         response = self.client.post("/201507/activities", auth=(self.username, self.password),
-                                    data=json.dumps(json_body),
+                                    data=json.dumps(self.json_body),
                                     headers=self.headers, catch_response=True)
         json_var = response.json()
         print(json_var['timestamp'])
@@ -35,14 +48,11 @@ class MyTaskSet(TaskSet):
             order_index = randint(1, user_order_count) - 1
             print("user " + self.users_list[user_index] + " purchase_order#" + self.order_list[user_index][order_index])
             print(self.order_list[user_index][order_index])
-
-
-            self.headers = {"accept-encoding": "gzip, deflate", "Accept": "application/json",
-                            "Content-Type": "application/json"}
-            json_body = {"activity": {"subscriber": {"uid": self.users_list[user_index]}, "event": "purchase",
-                                      "properties": {"order_id": self.order_list[user_index][order_index]}}}
+            self.json_body['uid'] = "user%i@test.com" % user_index
+            self.json_body['event'] = "purchase"
+            self.json_body['order_id'] = self.order_list[user_index][order_index]
             response = self.client.post("/201507/activities", auth=(self.username, self.password),
-                                        data=json.dumps(json_body),
+                                        data=json.dumps(self.json_body),
                                         headers=self.headers, catch_response=True)
             json_var = response.json()
             print(json_var['timestamp'])
